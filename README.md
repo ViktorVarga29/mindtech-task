71a579b0ce4f7604319d441e43c39c40089c6ff8
Varga Viktor <vik.varga97@gmail.com>
2025-08-28 23:56:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+I chose to prioritize making the functions work described in the task.
+For the sake of working fast I have used GO for the backend which I have used a lot with lately.
+For the frontend I've used React with JavaScript instead of TypeScript.
+
+1. Run in /backend
+```
+go run .
+```
+
+2. Run in /frontend
+```
+npm install
+npm run dev
+```
+
--- backend/main.go
+++ backend/main.go
@@ -12,11 +12,11 @@ func main() {
 	server := gin.Default()
 
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"}, // your frontend(s)
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: false, // set true only if you use cookies
+		AllowCredentials: false,
 	}))
 
 	routes.RegisterRoutes(server)
